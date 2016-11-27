39f9cc71f7b80b7e215a9159b95c40732d892d97
johngurt <johngurt@rambler.ru>
2016-11-27 20:38:55 +0200

    add Memento pattern

---
+++ Patterns/src/memento/MementoGame.java
@@ -0,0 +1,82 @@
+package memento;
+
+/**
+ * @author Hurt Yevhenii
+ */
+public class MementoGame {
+    public static void main(String[] args) {
+        Game game = new Game();
+        game.set("LVL_1", 30000);
+        System.out.println(game);
+
+        File file = new File();
+        file.setSave(game.save());
+
+        game.set("LVL_2", 55000);
+        System.out.println(game);
+
+        System.out.println("Loading");
+        game.load(file.getSave());
+        System.out.println(game);
+    }
+}
+
+//Originator
+class Game {
+    private String level;
+    private int ms;
+    public void set(String level, int ms) {
+        this.level = level;
+        this.ms = ms;
+    }
+
+    public void load(Save save) {
+        level = save.getLevel();
+        ms = save.getMs();
+    }
+
+    public Save save() {
+        return new Save(level, ms);
+    }
+
+    @Override
+    public String toString() {
+        return "Game{" +
+                "level='" + level + '\'' +
+                ", ms=" + ms +
+                '}';
+    }
+}
+
+//Memento
+class Save {
+    private final String level;
+    private final int ms;
+
+    public Save(String level, int ms) {
+
+        this.level = level;
+        this.ms = ms;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public int getMs() {
+        return ms;
+    }
+}
+
+//Caretaker
+class File {
+    Save save;
+
+    public Save getSave() {
+        return save;
+    }
+
+    public void setSave(Save save) {
+        this.save = save;
+    }
+}
