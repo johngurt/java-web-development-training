d1005eae97b93997a896cf45e0be1ea49881c06f
johngurt <johngurt@rambler.ru>
2016-12-01 17:08:37 +0200

    add Proxy pattern

---
+++ Patterns/src/proxy/ProxyApp.java
@@ -0,0 +1,54 @@
+package proxy;
+
+/**
+ * @author Hurt Yevhenii
+ */
+public class ProxyApp {
+
+    public static void main(String[] args) {
+
+        Image image = new ProxyImage("D:/images/my.img");
+        image.display();
+    }
+}
+
+interface Image {
+    void display();
+}
+
+class RealImage implements Image {
+
+    String file;
+    public RealImage(String file) {
+        this.file = file;
+        load();
+    }
+
+    void load() {
+        System.out.println("Loading " + file);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Displaying " + file);
+    }
+}
+
+class ProxyImage implements Image {
+
+    String file;
+    RealImage image;
+    public ProxyImage(String file) {
+        this.file = file;
+    }
+
+
+
+    @Override
+    public void display() {
+        if (image == null) {
+            image = new RealImage(file);
+        }
+        image.display();
+    }
+}
